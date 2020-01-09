ecc6d2db3704356901824cefb6b83b84e681a880
Matthew Torrence <matt@torrencefamily.net>
2020-01-09 16:03:28 -0500

    Add benchmark for fastset<u128>

--- addcomb-comp/benches/fastset_benches.rs
+++ addcomb-comp/benches/fastset_benches.rs
@@ -25,6 +25,9 @@ fn bench_nus(c: &mut Criterion) {
         group.bench_with_input(BenchmarkId::new("Fast", n), n, |b, n| {
             b.iter(|| nu::<FastSet>(black_box(*n), black_box(5), black_box(2), false))
         });
+        group.bench_with_input(BenchmarkId::new("Fast<u128>", n), n, |b, n| {
+            b.iter(|| nu::<FastSet<u128>>(black_box(*n), black_box(5), black_box(2), false))
+        });
         group.bench_with_input(BenchmarkId::new("Exact", n), n, |b, n| {
             b.iter(|| {
                 nu::<Vec<GElem>>(
@@ -50,6 +53,11 @@ fn bench_nus(c: &mut Criterion) {
                 nu_signed_restricted::<FastSet>(black_box(*n), black_box(5), black_box(2), false)
             })
         });
+        group.bench_with_input(BenchmarkId::new("Fast<u128>", n), n, |b, n| {
+            b.iter(|| {
+                nu_signed_restricted::<FastSet<u128>>(black_box(*n), black_box(5), black_box(2), false)
+            })
+        });
         group.bench_with_input(BenchmarkId::new("Exact", n), n, |b, n| {
             b.iter(|| {
                 nu_signed_restricted::<Vec<GElem>>(
@@ -65,17 +73,21 @@ fn bench_nus(c: &mut Criterion) {
     
     let mut group = c.benchmark_group("sumsets");
     let a_fast1: FastSet = (&[1u32, 3, 10, 11, 25]).into();
+    let a_fast1_lg: FastSet<u128> = (&[1u32, 3, 10, 11, 25]).into();
     let a_exact1: Vec<GElem> = vec![GElem(vec![1]), GElem(vec![3]), GElem(vec![10]), GElem(vec![11]), GElem(vec![25])];
     let a_fast2: FastSet = (&[1u32, 3, 10, 11, 25, 30, 50, 55, 58, 60]).into();
+    let a_fast2_lg: FastSet<u128> = (&[1u32, 3, 10, 11, 25, 30, 50, 55, 58, 60]).into();
     let a_exact2: Vec<GElem> = vec![GElem(vec![1]), GElem(vec![3]), GElem(vec![10]), GElem(vec![11]), GElem(vec![25]), GElem(vec![30]), GElem(vec![50]), GElem(vec![55]), GElem(vec![58]), GElem(vec![60])];
     let g_exact1 = Rc::new(vec![30]);
     let g_exact2 = Rc::new(vec![62]);
     group.sample_size(2000);
     group.bench_function("5-fold sumset of A, |A| = 5, fastset", |b| b.iter(|| black_box(a_fast1.hfold_sumset(black_box(5), 35))));
+    group.bench_function("5-fold sumset of A, |A| = 5, fastset<u128>", |b| b.iter(|| black_box(a_fast1_lg.hfold_sumset(black_box(5), 35))));
     group.sample_size(200);
     group.bench_function("5-fold sumset of A, |A| = 5, exactset", |b| b.iter(|| black_box(a_exact1.hfold_sumset(black_box(5), g_exact1.clone()))));
     group.sample_size(2000);
     group.bench_function("5-fold sumset of A, |A| = 10, fastset", |b| b.iter(|| black_box(a_fast2.hfold_sumset(black_box(5), 62))));
+    group.bench_function("5-fold sumset of A, |A| = 10, fastset<u128>", |b| b.iter(|| black_box(a_fast2_lg.hfold_sumset(black_box(5), 62))));
     group.sample_size(80);
     group.bench_function("5-fold sumset of A, |A| = 10, exactset", |b| b.iter(|| black_box(a_exact2.hfold_sumset(black_box(5), g_exact2.clone()))));
     group.finish();
