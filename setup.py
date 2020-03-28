6e12ee03d6a9267d5d6a5a795a95121c9533e45f
= <=>
2020-03-28 14:06:47 -0400

    bump version for peter

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ class cp_build_ext(build_ext):
               
 
 setup (name = 'addcomb',
-       version = '0.2.4',
+       version = '0.3',
        author = "Matt Torrence <gh-Torrencem>",
        description = """Fast Additive Combinatorics for use in Sage""",
        cmdclass = {
